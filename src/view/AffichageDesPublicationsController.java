365d41dc3c6bc2cfd0738241524b17246bc18271
mahmoudi-bilel <78540918+mahmoudi-bilel@users.noreply.github.com>
2023-04-21 06:29:32 +0200

    push after 2 API mailling and qrcode

--- src/view/AffichageDesPublicationsController.java
+++ src/view/AffichageDesPublicationsController.java
@@ -6,6 +6,7 @@
 package view;
 
 import entites.publication;
+import java.io.File;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -23,6 +24,19 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import service.publicationCrud;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
 
 /**
  * FXML Controller class
@@ -45,6 +59,10 @@ public class AffichageDesPublicationsController implements Initializable {
     private TextField rechercheTitre;
     @FXML
     private Button Rechercher;
+    @FXML
+    private Button butonnqr;
+    @FXML
+    private ImageView imageqr;
 
     /**
      * Initializes the controller class.
@@ -86,4 +104,43 @@ public class AffichageDesPublicationsController implements Initializable {
     ObservableList<publication> observableList = FXCollections.observableArrayList(publicationList);
     table.setItems(observableList);
     }
+
+    @FXML
+    private void qrcode(ActionEvent event) throws WriterException {
+         
+        publication selectedPiece = table.getSelectionModel().getSelectedItem();
+        if (selectedPiece != null) {
+            String qrText = selectedPiece.getDescription();
+            createQR(qrText);
+        } else {
+            alert("Please select a row.");
+        }
+    }
+    private void alert(String msg) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText(msg);
+        alert.showAndWait();
+    }
+    private void setQRImage(String path) {
+        try {
+            FileInputStream stream = new FileInputStream(path);
+            Image image = new Image(stream);
+            imageqr.setImage(image);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    private void createQR(String qrText) throws WriterException {
+        try {
+            String path = System.getProperty("user.home") + File.separatorChar + "Desktop" + File.separatorChar + "qr_code.png";
+            BitMatrix matrix = new MultiFormatWriter().encode(qrText, BarcodeFormat.QR_CODE, 200, 200);
+            MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
+            alert("QR Code Created");
+            setQRImage(path);
+            //hl.setVisible(false);
+        } catch (IOException | WriterException e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
